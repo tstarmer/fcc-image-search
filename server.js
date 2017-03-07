34891dbd798d8843f8d3c737688f9ba32d36a381
Timothy Starmer <tim.starmer@gmail.com>
2017-03-07 23:51:25 +0000

    connection to mlab working

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-var mongodb = require('mongodb').MongoClient;
-var express = require('express');
-var app = express();
+const mongodb = require('mongodb').MongoClient;
+const express = require('express');
+const app = express();
+
+const mongoUrl = process.env.MONGOLAB_URI;
+
+//init db
+function initDb(item){
+    mongodb.connect(mongoUrl, function(err,db){
+        if(err){
+            console.log(err)
+        }else{
+            var ltSearches = db.collection("latest-search")
+           
+            ltSearches.insertOne({"something2":"another-something"});
+           
+            db.close();
+            
+        }
+    })
+}
 
 
-app.get("/", function(req,res){
-    
+app.get("/api/imagesearch/", function(req,res){
+    console.log("imagesearch")
+    res.send("query")
 });
+
+app.get("/api/latest/imagesearch", function(req,res){
+    console.log("checking latest")
+    initDb("mortise")
+    res.send("latest")
+});
+
+
 
 app.listen(process.env.PORT || 8080, function(){
   console.log('App listening on port ', process.env.PORT);
